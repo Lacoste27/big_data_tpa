6ae619d7b21063e2468f69f9ae5d3cb1716d7983
Robsona Tsiory <robsonatsiory07@gmail.com>
2024-05-05 20:41:26 +0300

    Update analyse.R

--- analyse/analyse.R
+++ analyse/analyse.R
@@ -1,8 +1,13 @@
-library(JDBC)
-hive_jdbc_jar <- "/usr/local/hive/jdbc/hive-jdbc-3.1.3-standalone.jar"
+library(RJDBC)
+hive_jdbc_jar <- "C:/Vm/INSTALL_MV_BIGDATA_BOX/lib/hive-jdbc-3.1.3-standalone.jar"
 hive_driver <- "org.apache.hive.jdbc.HiveDriver"
 hive_url <- "jdbc:hive2://localhost:10000"
 drv <- JDBC(hive_driver, hive_jdbc_jar)
-conn <- dbConnect(drv, hive_url, "vagrant", "")
-show_databases <- dbGetQuery(conn, "show databases")
-print(show_databases);
+conn <- dbConnect(drv, hive_url, "oracle", "welcome1")
+marketing <- dbGetQuery(conn, "SELECT * FROM concessionaire.marketing_ext")
+catalogue <- dbGetQuery(conn, "SELECT * FROM concessionaire.catalogue_ext")
+View(catalogue)
+table(catalogue$catalogue_ext.marque)
+pie(table(catalogue$catalogue_ext.occasion), main = "Repartition des classes")
+qplot(catalogue$catalogue_ext.marque, data=catalogue, fill=catalogue$catalogue_ext.occasion)
+table(catalogue$catalogue_ext.occasion)
